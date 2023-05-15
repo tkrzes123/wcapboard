a88bb10984cdbcc164b2edb8e80d24656ac961cb
tkr <tkr@test>
2023-05-15 20:31:50 +0200

    add ScoreBoard implementation

--- scoreboard/src/main/java/pl/sport/tk/scoreboard/model/BoardGame.java
+++ scoreboard/src/main/java/pl/sport/tk/scoreboard/model/BoardGame.java
@@ -1,8 +1,20 @@
 package pl.sport.tk.scoreboard.model;
 
 import java.time.Instant;
+import java.util.Comparator;
+
+import static java.util.Comparator.comparing;
 
 public interface BoardGame {
+
+    Comparator<BoardGame> SCORE_COMPARATOR =
+            comparing(BoardGame::getTotalScore).thenComparing(BoardGame::getStartTime);
+
+    Comparator<BoardGame> START_TIME_COMPARATOR = comparing(BoardGame::getStartTime);
+
+    private int getTotalScore() {
+        return getScore().getTotalScoreValue();
+    }
 
     Game getGame();
 
